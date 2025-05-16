b04314c5a5fb25097e56eaf056f3c6b0e21c7909
RingoKojima <105334720@student.swin.edu.au>
2025-05-16 15:54:44 +1000

    Timer
    Added a Timer function.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,19 +1,40 @@
 using UnityEngine;
+using UnityEngine.CoreModule;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject _boss;
     public GameObject _player;
+    public float _OriginalTimer;
+    private float _CountUp;
+    private bool EndGame = false; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _OriginalTimer == 99 
+        _CountUp == 0 
+    }
+
+    void Reset()
+    {
+        _CountUp = 0;
+        EndGame = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (EndGame)
+        {
+            _CountUp += Time.deltaTime;
+        }
+
+        if (_CountUp >= _OriginalTimer)
+            { 
+        EndGame = true;
+        }
+           
+
     }
 }
