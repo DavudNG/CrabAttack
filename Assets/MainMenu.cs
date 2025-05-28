4b6361e0f6f5550c6ed16722c8bc089add1ef392
RingoKojima <105334720@student.swin.edu.au>
2025-05-28 11:32:14 +1000

    Added sound affects

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource ButtonSound;
+
+
     public void PlayGame()
     {
+        ButtonSound.pitch = Random.Range(0.9f, 1.1f)
+        ButtonSound.Play();
         SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
     {
+        ButtonSound.Play();
         Application.Quit();
     }
 }
